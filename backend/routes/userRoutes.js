f598b2925358fbf4600b4127fb18185f1bd02b42
syahrilhanla <syahrilhanla5@gmail.com>
2022-06-18 08:39:02 +0800

    setup initial routes for /user

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const {
+	registerUser,
+	loginUser,
+	getUserData,
+} = require("../controllers/userController.js");
+
+router.post("/", registerUser);
+router.post("/login", loginUser);
+router.get("/me", getUserData);
+
+module.exports = router;
